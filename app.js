8a8138bd6fd353d15fee43ebf1635fe8048b6bbc
Alexey Zhirkov <avzhirkov@gmail.com>
2022-04-06 16:03:18 +0300

    add animals hbs, routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mainRoutes = require('./routes/main.routes');
+const animalsRoutes = require('./routes/animals.routews');
 
 const app = express();
 
@@ -7,6 +8,7 @@ const expressConfig = require('./config/express_config');
 expressConfig(app);
 
 app.use('/', mainRoutes);
+app.use('/animals', animalsRoutes);
 
 app.listen(3000, () => {
   console.log('Server start ))');
