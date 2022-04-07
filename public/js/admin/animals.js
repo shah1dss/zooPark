def90e02f4438f91ad2cde791010cfa086416df1
Maks <dyupriz89evms@mail.ru>
2022-04-07 10:10:52 +0300

    model window

--- public/js/admin/animals.js
+++ public/js/admin/animals.js
@@ -1,5 +1,8 @@
-const listAnimals = document.querySelector('#list-animals-div');
+// const async = require("hbs/lib/async");
+
 const cardForm = document.querySelector('#card-form');
+const listAnimals = document.querySelector('#list-animals-div');
+const editForm = document.querySelector('#edit-form');
 const errorDiv = document.querySelector('#error-div');
 
 cardForm?.addEventListener('submit', async (event) => {
@@ -36,14 +39,15 @@ cardForm?.addEventListener('submit', async (event) => {
 });
 
 listAnimals.addEventListener('click', async (event) => {
-  event.preventDefault();
-  const action = event.target.href;
-
-  if (action) {
+  const { del, edit } = event.target.dataset;
+  if (event.target.dataset.del === del && del) {
     try {
-      const response = await fetch(action, {
+      const response = await fetch(`/admin/animals/${del}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          del,
+        }),
       });
 
       const data = await response.text();
@@ -52,10 +56,65 @@ listAnimals.addEventListener('click', async (event) => {
         errorDiv.innerHTML = '<h3> сервер Ошибка при удаление! </h3>';
       } else {
         listAnimals.innerHTML = data;
+        errorDiv.innerHTML = '';
       }
     } catch (error) {
       console.log(error);
       errorDiv.innerHTML = '<h2> клиент Ошибка при удаление! </h2>';
     }
+  } else if (event.target.dataset.edit === edit && edit) {
+    editForm.addEventListener('submit', async (events) => {
+      events.preventDefault();
+      try {
+        const { name, photo, description } = events.target;
+
+        const response = await fetch(`/admin/animals/${edit}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            edit,
+            name: name.value,
+            photo: photo.value,
+            description: description.value,
+          }),
+        });
+
+        const data = await response.text();
+
+        if (data === 'ошибка при изменении') {
+          errorDiv.innerHTML = '<h3> сервер не Ошибка при изменении! </h3>';
+        } else {
+        // listAnimals.innerHTML = data;
+          errorDiv.innerHTML = '';
+        }
+      } catch (error) {
+        console.log(error);
+        errorDiv.innerHTML = '<h2> клиент Ошибка при изменении! </h2>';
+      }
+    });
   }
 });
+// listAnimals.addEventListener('click', async (event) => {
+//   event.preventDefault();
+//   const action = event.target.href;
+
+//   if (action) {
+//     try {
+//       const response = await fetch(action, {
+//         method: 'DELETE',
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+
+//       const data = await response.text();
+
+//       if (data === 'ошибка при удаление') {
+//         errorDiv.innerHTML = '<h3> сервер Ошибка при удаление! </h3>';
+//       } else {
+//         listAnimals.innerHTML = data;
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       errorDiv.innerHTML = '<h2> клиент Ошибка при удаление! </h2>';
+//     }
+//   }
+// });
--- routes/admin/animals.routes.js
+++ routes/admin/animals.routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const async = require('hbs/lib/async');
+// const async = require('hbs/lib/async');
 // const async = require('hbs/lib/async');
 const { Animal } = require('../../db/models');
 
@@ -27,14 +29,50 @@ router.route('/')
 router.route('/:id')
   .delete(async (req, res) => {
     try {
-      const { id } = req.params;
-      await Animal.destroy({ where: { id } });
+      const { del } = req.body;
+      await Animal.destroy({ where: { id: del } });
       const allAnimals = await Animal.findAll({ order: [['createdAt', 'DESC']] });
       res.render('admin/listAnimalsAdmin', { allAnimals, layout: false });
     } catch (error) {
       console.log(error);
       res.send('ошибка при удаление');
     }
+  })
+  .put(async (req, res) => {
+    const {
+      edit, name, photo, description,
+    } = req.body;
+    const editAnimals = await Animal.update(
+      { name, photo, description },
+      { where: { id: edit }, raw: true },
+    );
+    res.send('ошибка при изменении');
   });
+
+// router.route('/:id')
+//   .delete(async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       await Animal.destroy({ where: { id } });
+//       const allAnimals = await Animal.findAll({ order: [['createdAt', 'DESC']] });
+//       res.render('admin/listAnimalsAdmin', { allAnimals, layout: false });
+//     } catch (error) {
+//       console.log(error);
+//       res.send('ошибка при удаление');
+//     }
+//   });
+
+// router.route('/:id/edit')
+//   .delete(async (req, res) => {
+//     res.render('admin/editAnimalsAdmin', { layout: false });
+//   });
+// .put(async (req, res) => {
+//   try {
+
+//   } catch (error) {
+//     console.log(error);
+//     res.send('ошибка при изменении');
+//   }
+// });
 
 module.exports = router;
