59ab07fc039b2ba6da51788aee9bcd64be59a176
Maks <dyupriz89evms@mail.ru>
2022-04-08 11:29:48 +0300

    актуальная

--- public/application.js
+++ public/application.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const html = await fetch('/tariff/getTariffs');
   const data = await html.text();
-
   const dropdownList = document.querySelector('#dropdown_id');
   dropdownList.innerHTML = data;
 });
